d62060d2f7cc23a59e88f2ad2a5f3c19ba11e008
Gregory Petukhov <lorien@lorien.name>
2015-08-10 19:38:38 +0600

    Bump version: 0.0.2 → 0.0.3

--- captcha_solver/__init__.py
+++ captcha_solver/__init__.py
@@ -1,4 +1,4 @@
 from captcha_solver.solver import CaptchaSolver  # noqa
 from captcha_solver.error import *  # noqa
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ ROOT = os.path.dirname(os.path.realpath(__file__))
 
 setup(
     name = 'captcha-solver',
-    version = '0.0.2',
+    version = '0.0.3',
     description = 'Universal API to different captcha solving services',
     long_description = open(os.path.join(ROOT, 'README.rst')).read(),
     author = 'Gregory Petukhov',
