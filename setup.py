7e82f8cb1d2f33d58f88031d463f53c3d363a108
Gregory Petukhov <lorien@lorien.name>
2019-01-13 01:51:14 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,18 @@ ROOT = os.path.dirname(os.path.realpath(__file__))
 setup(
     name = 'captcha-solver',
     version = '0.1.1',
-    description = 'Universal API to captcha solving services',
-    long_description = open(os.path.join(ROOT, 'README.rst')).read(),
     author = 'Gregory Petukhov',
     author_email = 'lorien@lorien.name',
+    maintainer='Gregory Petukhov',
+    maintainer_email='lorien@lorien.name',
+    url='https://github.com/lorien/captcha_solver',
+    description = 'Universal API to captcha solving services',
+    long_description = open(os.path.join(ROOT, 'README.rst')).read(),
     packages = find_packages(exclude=['test']),
+    download_url='https://github.com/lorien/captcha_solver/releases',
     license = "MIT",
     install_requires = ['six'],
+    keywords='captcha antigate rucaptcha 2captcha',
     classifiers = [
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
@@ -22,7 +27,6 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
         'License :: OSI Approved :: MIT License',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
